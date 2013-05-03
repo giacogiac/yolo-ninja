f6ec2062383757ea6cd10f2bde0a1744ffc2b932
giacogiac <giacomo.rombaut@gmail.com>
2013-05-03 15:35:03 +0200

    Bouton arrondi

--- Barker/src/client/Main.java
+++ Barker/src/client/Main.java
@@ -1,8 +1,18 @@
 package client;
 
+import javax.swing.JWindow;
+import javax.swing.SwingUtilities;
+
+
 public class Main {
 
 	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run(){
+				MainWindow window = new MainWindow("Barker");
+				window.setVisible(true);//On la rend visible
+			}
+		});
 		
 	}
 
